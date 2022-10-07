97d58a5d29349f6b0685faa1a5726fc18c2ed8c8
unknown <anthony.prunier021@gmail.com>
2022-10-07 14:30:28 +0200

    qr-scanner

--- qcm1.php
+++ qcm1.php
@@ -169,4 +169,51 @@ else
     header("Location: redirect.php");
 
 }
+/*
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Refresh"/>
+</head>
+<h1 style="text-align: center;">Scanner le Qr Code du prochain stand</h1>
+<hr>
 
+<script src="js/html5-qrcode.min.js"></script>
+<style>
+  .result{
+    background-color: green;
+    color:#fff;
+    padding:20px;
+  }
+  .row{
+    display:flex;
+}
+</style>
+
+
+<div class="row">
+  <div class="col">
+    <div style="width:500px;" id="reader"></div>
+  </div>
+  <div class="col" style="padding:30px;">
+    <div id="result"></div>
+  </div>
+</div>
+
+
+<script type="text/javascript">
+function onScanSuccess(qrCodeMessage) {
+//    document.getElementById('result').innerHTML = '<p>Vers <a href='+qrCodeMessage+' >stand 2</a>.</p>';
+    document.getElementById('result').innerHTML = '<meta http-equiv="refresh" content="0; URL='+qrCodeMessage+'">';
+}
+
+function onScanError(errorMessage) {
+    //handle scan error
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+</script>
+</html>*/
